8986754673b0805633c4b19c36af3aa3bd2bb3f3
Dmitriy Kosterev <dd9040055998@gmail.com>
2024-03-19 00:18:24 +0300

    Добавил исключение ошибок

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,9 +6,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
     @Test
-    void SuccessfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+    void successfulSearchTest() {
+        try {
+            open("https://www.google.com/");
+            $("[name=q]").setValue("selenide").pressEnter();
+            $("[id=search]").shouldHave(text("https://selenide.org"));
+        } catch (Exception e) {
+            System.out.println("Опять произошла ошибка: " + e.getMessage());
+        }
     }
 }
